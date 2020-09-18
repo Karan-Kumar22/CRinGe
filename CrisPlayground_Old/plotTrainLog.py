417c0bfdab94b5cc9a8c774d48da848cbfff88b9
Karan-Kumar22 <66718437+Karan-Kumar22@users.noreply.github.com>
2020-09-18 00:01:54 -0400

    Add files via upload

---
+++ CrisPlayground_Old/plotTrainLog.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+
+testEpoch = []
+testLoss = []
+
+trainEpoch = []
+trainLoss = []
+
+lines = [line.strip('\n').split() for line in open('Training_data_30N_LeakyRelu.txt')]
+
+for line in lines :
+    if line[0] == 'TRAINING' :
+        trainEpoch.append(float(line[4]))
+        trainLoss.append(float(line[6]))
+    elif line[0] == "VALIDATION" :
+        testEpoch.append(float(line[4]))
+        testLoss.append(float(line[6]))
+
+
+plt.plot(trainEpoch, trainLoss, color = 'b', alpha = 0.5, label = "Train")
+plt.plot(testEpoch, testLoss, color = 'r', alpha = 0.5, label = "Test")
+
+plt.legend()
+
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+
+#plt.yscale('log')
+
+plt.tight_layout()
+
+plt.savefig("trainLog.png")
+
+
+plt.show()
